843991f922e008e146d5d76771f8e2fb3ee1d94a
Daniel Santos <79427618+danielthejoker18@users.noreply.github.com>
2022-05-11 20:58:16 -0300

    adiantamento da questão um

--- main.java
+++ main.java
@@ -1,8 +1,43 @@
 public class Class {
     public static void main(String[] args) {
-	import java.util.Scanner;
-    Scanner ler = new Scanner(System.in);
-    int n;
-    n = ler.nextInt();
-}
+        import java.util.Scanner;
+        Scanner ler = new Scanner(System.in);
+        Pessoa[] pessoas = new Pessoa[3];
+        for(int = 0; i < 3; i++){
+            pessoa[i] = new Pessoa();
+            System.out.println("Entre com o nome da pessoa ".i+1);
+            pessoas[i].getNome(ler.nextline());
+            ler.nextLine();
+        }
+    }
+    class Pessoa{
+        private String nome;
+        private int idade;
+        private String cpf;
+
+            public String getNome() {
+                return nome;
+            }
+
+            public void setNome(String nome) {
+                this.nome = nome;
+            }
+
+            public int getIdade() {
+                return idade;
+            }
+
+            public void setIdade(int idade) {
+                this.idade = idade;
+            }
+
+            public String getCpf() {
+                return cpf;
+            }
+
+            public void setCpf(String cpf) {
+                this.cpf = cpf;
+            }
+        
+    }
 }
